6d4af156422f6d199880a4586239cab28d8f9aa2
Hwan Kim <hwankim1941@gmail.com>
2022-11-27 19:07:30 +0900

    init project

---
+++ src/main/java/com/prgrms/merskkurly/MerskKurlyApplication.java
@@ -0,0 +1,13 @@
+package com.prgrms.merskkurly;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MerskKurlyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MerskKurlyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/prgrms/merskkurly/MerskKurlyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prgrms.merskkurly;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MerskKurlyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
