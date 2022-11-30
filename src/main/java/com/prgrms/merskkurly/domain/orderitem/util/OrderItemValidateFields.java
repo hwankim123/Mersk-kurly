0d53fb4183757dd5ca781ea942ccbb524ad126d1
hwankim123 <hwankim1941@gmail.com>
2022-11-30 18:53:10 +0900

    [KER-01] feat: OrderItem 개발

---
+++ src/main/java/com/prgrms/merskkurly/domain/orderitem/util/OrderItemValidateFields.java
@@ -0,0 +1,7 @@
+package com.prgrms.merskkurly.domain.orderitem.util;
+
+public class OrderItemValidateFields {
+  public static final String QUANTITY_FIELD_NAME = "quantity";
+  public static final int MIN_QUANTITY = 1;
+
+}
