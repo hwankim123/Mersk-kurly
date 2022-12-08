1d88c55e3452e577f24f90f405bed544350de3e8
hwankim123 <hwankim1941@gmail.com>
2022-12-08 16:32:11 +0900

    [KER-02] feat: Exception 클래스 추가.

---
+++ src/main/java/com/prgrms/merskkurly/domain/common/auth/SessionAttributes.java
@@ -0,0 +1,7 @@
+package com.prgrms.merskkurly.domain.common.auth;
+
+public class SessionAttributes {
+  public static final String ID = "id";
+  public static final String NAME = "name";
+  public static final String ROLE = "role";
+}
